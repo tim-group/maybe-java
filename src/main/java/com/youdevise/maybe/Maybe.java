cd197ae6d3939f361722d13471eee10e605637dc
Tom Denley <tom.denley@youdevise.com>
2011-09-02 11:18:33 +0100

    java style changes -- always braces with if statements

--- src/main/java/com/youdevise/maybe/Maybe.java
+++ src/main/java/com/youdevise/maybe/Maybe.java
@@ -110,12 +110,15 @@ public abstract class Maybe<T> implements Iterable<T> {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
 
-            final DefiniteValue<?> that = (DefiniteValue<?>) o;
-
-            return theValue.equals(that.theValue);
+            return theValue.equals(((DefiniteValue<?>)o).theValue);
         }
 
         @Override
